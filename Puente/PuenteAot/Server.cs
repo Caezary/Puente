6fb7515b0f63ad41424dad18da4a3c290141f15d
Caezary <cezary.komarzeniec@gmail.com>
2023-10-04 01:34:37 +0200

    add Puente dotnet projects

---
+++ Puente/PuenteAot/Server.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PuenteAot;
+
+public static class Server
+{
+    [UnmanagedCallersOnly(EntryPoint = "addNumbers")]
+    public static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
+
---
+++ Puente/PuenteCom/Server.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PuenteCom;
+
+[ComVisible(true)]
+[Guid("b022d7ea-1dfb-4911-9d53-a26bc8fc9674")]
+[InterfaceType(ComInterfaceType.InterfaceIsDual)]
+public interface IServer
+{
+    int AddNumbers(int a, int b);
+}
+
+[ComVisible(true)]
+[Guid("af900b10-ff4b-41e6-9d1b-8f95d6d7d3bb")]
+public class Server : IServer
+{
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
